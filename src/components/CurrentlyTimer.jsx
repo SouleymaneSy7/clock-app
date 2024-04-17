f31fc104c2872008c621e56fa1bc57eb77651f99
Souleymane Sy <souleymanesyservices@gmail.com>
2024-04-18 01:01:14 +0200

    feat: add location and country code for the users

--- src/components/CurrentlyTimer.jsx
+++ src/components/CurrentlyTimer.jsx
@@ -1,16 +1,67 @@
+import axios from "axios";
+import { useState, useEffect } from "react";
+
 export const CurrentlyTimer = () => {
-  const currentTime = new Date();
+  const LOCATION_REQUEST_URL = "https://api.ipbase.com/v1/json/";
+  const [countryName, setCountryName] = useState(null);
+  const [countryCode, setCountryCode] = useState(null);
 
-  const offset = currentTime.getTimezoneOffset();
-  const local = new Date(currentTime.getTime() + offset * 60000);
+  let currentTime = new Date();
 
-  let Hours = local.getHours();
-  let Minutes = local.getMinutes();
+  let TimezoneOffset = currentTime.getTimezoneOffset();
+  let localTime = new Date(currentTime.getTime() + TimezoneOffset * 60000);
 
+  let Hours = localTime.getHours();
+  let Minutes = localTime.getMinutes();
+
+  // Location Request
+  const getLocation = () => {
+    axios({
+      method: "get",
+      url: LOCATION_REQUEST_URL,
+      responseType: "json",
+    })
+      .then(function (response) {
+        const location = response.data;
+
+        setCountryName(location.country_name);
+        setCountryCode(location.country_code);
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+  };
+
+  useEffect(() => {
+    let onMounted = true;
+
+    if (onMounted) {
+      getLocation();
+    }
+
+    return () => {
+      onMounted = false;
+    };
+  }, []);
 
   return (
-    <h1>
-      <span> {Hours} </span> <span> {Minutes} </span>
-    </h1>
+    <section className="currently-timer">
+      <div className="currently-timer__greeting">
+        <span className="greeting__text"> Good Morning , </span>
+        <span className="greeting__currently">it&apos;s Currently</span>
+      </div>
+
+      <div className="currently-timer__timers">
+        <h1 className="timers__text">
+          {Hours} : {Minutes}
+        </h1>
+      </div>
+
+      <div className="currently-timer__location">
+        <p className="location__text">
+          In {countryName}, {countryCode}
+        </p>
+      </div>
+    </section>
   );
 };
