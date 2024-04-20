28e32118f639328820fe01b0198071898b3f1f0f
Souleymane Sy <souleymanesyservices@gmail.com>
2024-04-20 06:28:58 +0200

    feat: add features for the users to see realtime weather informations

--- src/components/Meteo.jsx
+++ src/components/Meteo.jsx
@@ -1,3 +1,62 @@
+/* eslint-disable react-hooks/exhaustive-deps */
+import axios from "axios";
+import { useState, useEffect } from "react";
+
 export const Meteo = () => {
-  return <p>Hello Meteo</p>
+  const [latitude, setLatitude] = useState(null);
+  const [longitude, setLongitude] = useState(null);
+  const [weatherData, setWeatherData] = useState([]);
+
+  useEffect(() => {
+    const getWeatherData = () => {
+      navigator.geolocation.getCurrentPosition(function (position) {
+        setLatitude(position.coords.latitude);
+        setLongitude(position.coords.longitude);
+      });
+
+
+      axios
+        .get(
+          `${
+            import.meta.env.VITE_WEATHER_REQUEST_URL
+          }?location=${latitude},${longitude}&apikey=${
+            import.meta.env.VITE_WEATHER_API_KEY
+          }`
+        )
+        .then(function (response) {
+          const wData = response.data;
+
+          setWeatherData(wData.data.values);
+          console.log(wData);
+        })
+        .catch(function (error) {
+          console.log(error);
+        });
+    };
+
+    getWeatherData();
+  }, [latitude, longitude]);
+
+  return (
+    <div>
+      <div>
+        <img
+          src={`${weatherData.weatherCode}_clear_small.png`}
+          alt="Powered by Tomorrow.io"
+        />
+      </div>
+      <div>
+        Temperature: {weatherData.temperature} <sup>&deg;C</sup>{" "}
+      </div>
+      <div>
+        Wind Speed: {weatherData.windSpeed} <span>mph</span>{" "}
+      </div>
+      <div>
+        Humidity: {weatherData.humidity} <span>%</span>{" "}
+      </div>
+      <div>
+        Feelings: {weatherData.temperatureApparent} <sup>&deg;C</sup>{" "}
+      </div>
+    </div>
+  );
 };
